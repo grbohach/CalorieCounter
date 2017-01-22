e1a35f816591a0de0ef86e94798aae1d3166cd3e
grbohach <grbohach@coe.edu>
2017-01-22 17:36:54 -0600

    Initial Commit

---
+++ app/src/main/java/com/example/user/caloriecounter/CalorieCounter.java
@@ -0,0 +1,13 @@
+package com.example.user.caloriecounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalorieCounter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calorie_counter);
+    }
+}
